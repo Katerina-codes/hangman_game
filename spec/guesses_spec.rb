52f84d22c2f088354838d8831e09e2901a03d3c8
Katerina <katerinaldg@gmail.com>
2017-05-11 11:45:18 +0100

    Refactored tests

--- spec/guesses_spec.rb
+++ spec/guesses_spec.rb
@@ -2,14 +2,14 @@ require 'guesses'
 
 describe Guesses do
 
+  subject(:guesses) { described_class.new }
+
   it "returns true if guessed letter is in word" do
-    guesses = Guesses.new
-    expect(guesses.check_guess("a", "nutella")).to eq(true)
+    expect(subject.check_guess("a", "nutella")).to eq(true)
   end
 
   it "returns false if guessed letter isn't inside word" do
-    guesses = Guesses.new
-    expect(guesses.check_guess("b", "nutella")).to eq(false)
+    expect(subject.check_guess("b", "nutella")).to eq(false)
   end
 
 end
