b728d45ae3530f8e808ae96bf3da4bac57468526
Katerina <katerinaldg@gmail.com>
2017-05-11 09:28:50 +0100

    Refactored tests to make them shorter

--- spec/word_places_spec.rb
+++ spec/word_places_spec.rb
@@ -2,19 +2,18 @@ require 'word_places'
 
 describe WordPlaces do
 
+  subject(:word_places) { described_class.new }
+
   it "returns one line for one character" do
-    word_places = WordPlaces.new
-    expect(word_places.spaces("a")).to eq("_")
+    expect(subject.spaces("a")).to eq("_")
   end
 
   it "returns two lines for two characters" do
-    word_places = WordPlaces.new
-    expect(word_places.spaces("aa")).to eq(["_","_"])
+    expect(subject.spaces("aa")).to eq(["_","_"])
   end
 
   it "returns three lines for three characters" do
-    word_places = WordPlaces.new
-    expect(word_places.spaces("aaa")).to eq(["_", "_", "_"])
+    expect(subject.spaces("aaa")).to eq(["_", "_", "_"])
   end
 
 end
