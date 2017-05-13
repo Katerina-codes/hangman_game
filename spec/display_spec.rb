f1d709766e2fc1bca013a0b636bcde2028848d95
Katerina <katerinaldg@gmail.com>
2017-05-13 12:05:15 +0100

    Refactored tests

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -1,16 +1,16 @@
 require 'display'
 
 describe Display do
+  let(:output) { StringIO.new }
 
   it "displays characters to the user" do
-    output = StringIO.new
     display = Display.new(output)
     display.display_lines("_")
     expect(output.string).to eq("Word = _\n")
   end
 
   it "asks for user to enter a letter" do
-    output = StringIO.new
+    # output = StringIO.new
     display = Display.new(output)
     display.ask_for_letter
     expect(output.string).to eq("Please enter a letter\n")
