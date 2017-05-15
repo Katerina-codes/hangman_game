1e9cb5a62ab3adf777e24cb3c30080cd4a706be9
Katerina <katerinaldg@gmail.com>
2017-05-15 15:59:09 +0100

    Only count incorrect guesses

--- lib/game_flow.rb
+++ lib/game_flow.rb
@@ -18,12 +18,12 @@ def game_flow(word)
   until guess_number == 6
     @display.ask_for_letter
     letter = @display.get_letter_input
-    guess_number += guesses.increment_guess(0)
     guess_checker = guesses.check_guess(letter, word)
     if guess_checker
       newest_word = word_places.substitute_letters(letter, lines, word)
       @display.display_lines(newest_word)
     else
+      guess_number += guesses.increment_guess(0)
       body_part = @display.draw_body_part(guess_number)
       @display.display_body_part(body_part)
     end
