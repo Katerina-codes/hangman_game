e06c608ba7eee53c0eea323e9ed8b23d801130bb
Katerina <katerinaldg@gmail.com>
2017-05-16 09:30:25 +0100

    Runs game

--- hangman_game_runner.rb
+++ hangman_game_runner.rb
@@ -1,10 +1,11 @@
 require_relative 'lib/word_places'
 require_relative 'lib/display'
+require_relative 'lib/guesses'
+require_relative 'lib/game_flow'
 
 word_places = WordPlaces.new
 display = Display.new
+guesses = Guesses.new
+game_flow = GameFlow.new
 
-lines = word_places.spaces("nutella")
-display.display_lines(lines)
-newest_word = word_places.substitute_letters("l" , lines, "nutella")
-display.display_lines(newest_word)
+game_flow.game_flow("nutella")
