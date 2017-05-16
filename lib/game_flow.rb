b8f1d5179cd280c775bf6d33f711ae228e1e67ac
Katerina <katerinaldg@gmail.com>
2017-05-16 10:23:08 +0100

    Removed code which displayed the word spaces twice

--- lib/game_flow.rb
+++ lib/game_flow.rb
@@ -17,18 +17,19 @@ def game_flow(word)
   newest_word = []
 
   until newest_word == word || guess_number == 6
+    @display.display_lines(lines)
     @display.ask_for_letter
     letter = @display.get_letter_input
     guess_checker = guesses.check_guess(letter, word)
     if guess_checker
       newest_word = word_places.substitute_letters(letter, lines, word)
-      @display.display_lines(newest_word)
     else
       guess_number += guesses.increment_guess(0)
       body_part = @display.draw_body_part(guess_number)
       @display.display_body_part(body_part)
     end
   end
+  @display.display_lines(newest_word)
   newest_word
 end
 
