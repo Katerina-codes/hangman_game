f82cef5620f9f1f26bc604d6d4d7876327e02174
Katerina <katerinaldg@gmail.com>
2017-05-16 17:38:48 +0100

    Removed dead code

--- lib/guesses.rb
+++ lib/guesses.rb
@@ -4,10 +4,6 @@ class Guesses
     word.include?(guess)
   end
 
-  def guess_number(number_of_guesses)
-    number_of_guesses
-  end
-
   def increment_guess(number_of_guesses)
     number_of_guesses += 1
   end
